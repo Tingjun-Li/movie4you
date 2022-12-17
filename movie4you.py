62c1e961fad103bc402336d88d3b98145a4227cc
李小钧 <tingjunl@umich.edu>
2022-12-17 00:03:54 -0500

    Modified movie4you.py to read JSON tree file

--- movie4you.py
+++ movie4you.py
@@ -4,7 +4,8 @@ import movie_tree
 import params
 from utils import open_cache, retrieve_and_save_movie_data
 
-app = Flask(__name__, template_folder='templates', static_folder='statics')
+app = Flask(__name__, template_folder="templates", static_folder="statics")
+
 
 def main():
     """
@@ -12,7 +13,9 @@ def main():
     """
     # Retrieve the movie data from the API
     if params.RETRIEVE_DATA:
-        retrieve_and_save_movie_data(params.MOVIE_LIST_FILENAME, popularity_threshold=20)
+        retrieve_and_save_movie_data(
+            params.MOVIE_LIST_FILENAME, popularity_threshold=20
+        )
 
     cache_dict_final = open_cache(params.CACHE_FILENAME)
     movie_cache_final = cache_dict_final.get("movie_details", [])
@@ -21,31 +24,29 @@ def main():
     # Load tree:
     if params.RESET_TREE:
         tree = movie_tree.buildTree(movie_cache_final, movie_tree.EMPTY_MOVIE_TREE)
-        tree_file_handle = open(params.TREE_FILE, "w")
-        movie_tree.saveTree(tree, tree_file_handle)
-        tree_file_handle.close()
+        movie_tree.saveTreeToJSON(tree, params.TREE_FILE_JSON)
     else:
-        tree_file_handle = open(params.TREE_FILE, "r")
-        tree = movie_tree.loadTree(tree_file_handle)
-        tree_file_handle.close()
+        tree = movie_tree.loadTreeFromJSON(params.TREE_FILE_JSON)
 
     # Find movies with tree:
     movie_tree.findMovies(tree, movie_cache_final)
+
 
 def get_movie_by_questionnaire(answers, number_of_movies):
     cache_dict_final = open_cache(params.CACHE_FILENAME)
     movie_cache_final = cache_dict_final.get("movie_details", [])
     print(f"The cache has {len(movie_cache_final)} qualified movies")
 
-    tree_file_handle = open(params.TREE_FILE, "r")
-    tree = movie_tree.loadTree(tree_file_handle)
-    tree_file_handle.close()
+    tree = movie_tree.loadTreeFromJSON(params.TREE_FILE_JSON)
 
     # Find movies with tree:
-    movie_result = movie_tree.findMovieByQuestionnaire(tree, movie_cache_final, number_of_movies, answers)
+    movie_result = movie_tree.findMovieByQuestionnaire(
+        tree, movie_cache_final, number_of_movies, answers
+    )
     return movie_result
 
-@app.route("/", methods = ["GET", "POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         print("POST")
@@ -60,11 +61,14 @@ def index():
 
         movie_result = get_movie_by_questionnaire(answers, number_of_movies)
         for movie in movie_result:
-            movie.rotten_tomatoes_url = params.ROTTEN_TOMATOES_URL + movie.title.lower().replace(": ","_").replace(" ", "_")
+            movie.rotten_tomatoes_url = (
+                params.ROTTEN_TOMATOES_URL
+                + movie.title.lower().replace(": ", "_").replace(" ", "_")
+            )
             print(movie.rotten_tomatoes_url)
 
         return render_template("result.html", movies=movie_result)
-    
+
     return render_template("index.html")
 
 
